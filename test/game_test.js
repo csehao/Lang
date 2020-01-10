8f87c781610c4fcbd314e0b2ecc9bc5759d393d7
Hao Wang <wanha@microsoft.com>
2020-01-09 16:18:08 -0800

    add mocha test

--- test/game_test.js
+++ test/game_test.js
@@ -1,8 +1,43 @@
 const assert = require('assert');
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const chai = require('chai');
+const expect = chai.expect;
+const { GameFactory } = require('../lib/game.js');
+
+function getDeathList(game) {
+    return game.characterList.map(character => character.dead);
+}
+
+describe('Reguar Setting Test', () => {
+    describe('Yu Nv Lie Shou Lang x4 Min X4', () => {
+        let game;
+        beforeEach(() => {
+            game = new GameFactory().createGame(
+                {
+                    characterCodeList: [
+                        "Y",
+                        "N",
+                        "Li",
+                        "S",
+                        "L",
+                        "L",
+                        "L",
+                        "L",
+                        "M",
+                        "M",
+                        "M",
+                        "M"
+                    ]
+                }
+            );
+        });
+        it('Lang Kill One Character Test', () => {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+            game.characterList[4].kill(0);
+            game.characterList[1].setDaoFa(1, 0);
+            game.judge.resolveStatus();
+            expect(getDeathList(game)).to.deep.equal(
+                [true, false, false, false, false, false, false, false, false, false, false, false]
+            );
+        });
     });
-  });
 });
