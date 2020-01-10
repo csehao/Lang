23d14998313d657e51b7939c41ff0fe7677ff172
Hao Wang <wanha@microsoft.com>
2020-01-09 19:05:09 -0800

    add test for guard

--- test/game_test.js
+++ test/game_test.js
@@ -9,26 +9,32 @@ function getDeathList(game) {
 
 describe('Reguar Setting Test', () => {
     describe('Yu Nv Lie Shou Lang x4 Min X4', () => {
+        const characterCodeList = [
+            "Y",
+            "N",
+            "Li",
+            "S",
+            "L",
+            "L",
+            "L",
+            "L",
+            "M",
+            "M",
+            "M",
+            "M"
+        ];
         let game;
-        beforeEach(() => {
-            game = new GameFactory().createGame(
+
+        const resetGame = () => {
+            game = GameFactory.createGame(
                 {
-                    characterCodeList: [
-                        "Y",
-                        "N",
-                        "Li",
-                        "S",
-                        "L",
-                        "L",
-                        "L",
-                        "L",
-                        "M",
-                        "M",
-                        "M",
-                        "M"
-                    ]
+                    characterCodeList
                 }
             );
+        };
+
+        beforeEach(() => {
+            resetGame();
         });
         it('Lang Kill One Character Test', () => {
             assert.equal([1, 2, 3].indexOf(4), -1);
@@ -40,7 +46,6 @@ describe('Reguar Setting Test', () => {
             );
         });
         it('Lang Kill One None Witch Character and Use Med Test', () => {
-            assert.equal([1, 2, 3].indexOf(4), -1);
             game.characterList[4].kill(0);
             game.characterList[1].fetchDaoFa();
             game.characterList[1].useMed();
@@ -48,6 +53,30 @@ describe('Reguar Setting Test', () => {
             expect(getDeathList(game)).to.deep.equal(
                 [false, false, false, false, false, false, false, false, false, false, false, false]
             );
+        });
+        it('Lang Kill One and Guard Guards Target Test', () => {
+            for (const killTarget in game.characterList) {
+                resetGame();
+                game.characterList[4].kill(killTarget);
+                game.characterList[3].guard(killTarget);
+                game.judge.resolveStatus();
+                expect(getDeathList(game)).to.deep.equal(
+                    [false, false, false, false, false, false, false, false, false, false, false, false]
+                );
+            }
+        });
+        it('Lang Kill One and Guard Guards Another Target Test', () => {
+            for (const index in game.characterList) {
+                resetGame();
+                const killTarget = parseInt(index); 
+                const guardTarget = ( killTarget + Math.floor(Math.random() * (characterCodeList.length - 2 ) + 1)) % characterCodeList.length;
+                game.characterList[4].kill(killTarget);
+                game.characterList[3].guard(guardTarget);
+                game.judge.resolveStatus();
+                const expectedResult = [false, false, false, false, false, false, false, false, false, false, false, false];
+                expectedResult[killTarget] = true;
+                expect(getDeathList(game)).to.deep.equal(expectedResult);
+            }
         });
     });
 });
