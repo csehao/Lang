86a2a2889d46b784cc972d8c9deb87e8a8cf1fa6
Hao Wang <wanha@microsoft.com>
2020-01-09 21:40:32 -0800

    add seer event

--- test/game_test.js
+++ test/game_test.js
@@ -36,8 +36,15 @@ describe('Reguar Setting Test', () => {
         beforeEach(() => {
             resetGame();
         });
+        it('Seer Reveal Test', () => {
+            for (const index in game.characterList) {
+                resetGame();
+                const revealTarget = parseInt(index); 
+                const faction = game.characterList[0].reveal(revealTarget).getRevealResult();
+                expect(faction).to.equal(game.judge.characters[revealTarget].faction);
+            }
+        });
         it('Lang Kill One Character Test', () => {
-            assert.equal([1, 2, 3].indexOf(4), -1);
             game.characterList[4].kill(0);
             game.characterList[1].fetchDaoFa();
             game.judge.resolveStatus();
@@ -46,17 +53,22 @@ describe('Reguar Setting Test', () => {
             );
         });
         it('Lang Kill One None Witch Character and Use Med Test', () => {
-            game.characterList[4].kill(0);
-            game.characterList[1].fetchDaoFa();
-            game.characterList[1].useMed();
-            game.judge.resolveStatus();
-            expect(getDeathList(game)).to.deep.equal(
-                [false, false, false, false, false, false, false, false, false, false, false, false]
-            );
+            for (const index in game.characterList) {
+                const killTarget = parseInt(index); 
+                resetGame();
+                game.characterList[4].kill(killTarget);
+                game.characterList[1].fetchDaoFa();
+                game.characterList[1].useMed();
+                game.judge.resolveStatus();
+                expect(getDeathList(game)).to.deep.equal(
+                    [false, false, false, false, false, false, false, false, false, false, false, false]
+                );
+            }
         });
         it('Lang Kill One and Guard Guards Target Test', () => {
-            for (const killTarget in game.characterList) {
+            for (const index in game.characterList) {
                 resetGame();
+                const killTarget = parseInt(index); 
                 game.characterList[4].kill(killTarget);
                 game.characterList[3].guard(killTarget);
                 game.judge.resolveStatus();
@@ -71,6 +83,21 @@ describe('Reguar Setting Test', () => {
                 const killTarget = parseInt(index); 
                 const guardTarget = ( killTarget + Math.floor(Math.random() * (characterCodeList.length - 2 ) + 1)) % characterCodeList.length;
                 game.characterList[4].kill(killTarget);
+                game.characterList[3].guard(guardTarget);
+                game.judge.resolveStatus();
+                const expectedResult = [false, false, false, false, false, false, false, false, false, false, false, false];
+                expectedResult[killTarget] = true;
+                expect(getDeathList(game)).to.deep.equal(expectedResult);
+            }
+        });
+        it('Lang Kill One and Guard Guards Target and Witch Med Test', () => {
+            for (const index in game.characterList) {
+                resetGame();
+                const killTarget = parseInt(index); 
+                const guardTarget = killTarget;
+                game.characterList[4].kill(killTarget);
+                game.characterList[1].fetchDaoFa();
+                game.characterList[1].useMed();
                 game.characterList[3].guard(guardTarget);
                 game.judge.resolveStatus();
                 const expectedResult = [false, false, false, false, false, false, false, false, false, false, false, false];
